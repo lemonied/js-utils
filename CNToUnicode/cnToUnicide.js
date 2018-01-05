dcd48247e7c7c529994391f29e75674dfce72425
papachen <447334358@qq.com>
2018-01-05 11:28:45 +0800

    'chen'

---
+++ CNToUnicode/cnToUnicide.js
@@ -0,0 +1,17 @@
+function tounicode(data) {
+    if(data === '') return '请输入汉字';
+    var str = '';
+    for(var i = 0; i < data.length; i++) {
+        str += "\\u" + parseInt(data[i].charCodeAt(0), 10).toString(16);
+    }
+    return str;
+}
+function tohanzi(data) {
+    if(data === '') return '请输入十六进制unicode';
+    data = data.split('\\u');
+    var str = '';
+    for(var i = 0; i < data.length; i++) {
+        str += String.fromCharCode(parseInt(data[i], 16).toString(10));
+    }
+    return str;
+}
